934c3fe5fc012e7356ccc720301be54a4074440f
shafeeq777444 <kp.shafeeqmohamed@gmail.com>
2025-05-10 08:30:22 +0530

    2

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,6 @@ export default defineConfig({
   server:{
     host:true,
     port:6000,
-    allowedHosts: ['app2.bidly.store', 'app3.bidly.store'],
+    allowedHosts: ['app2.bidly.store','app1.bidly.store', 'app3.bidly.store'],
   }
 })
